8db9495c62a2240f927500220ad9f24cdf4a3867
SilentCrouwd <chris.buchholz.lauenau@googlemail.com>
2026-08-20 14:49:02 +0200

    feat: add impressum page and update index layout with improved styling

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,14 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  base: "/restaurant_page",
+  base: "/restaurant_page/",
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: new URL("./index.html", import.meta.url).pathname,
+        impressum: new URL("./impressum.html", import.meta.url).pathname,
+      },
+    },
+  },
 });
